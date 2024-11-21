1e0cd99f613e41c7c8f291bd7c9dd30cc554239e
Student-developer-web <466085806@qq.com>
2024-11-21 19:22:44 +0800

    Add files via upload

---
+++ 周张图书馆/resources/writings/小鸡/scripts.js
@@ -0,0 +1,35 @@
+function changefontfamily(fontfamily)
+{
+    var writing=document.getElementById("text");
+    writing.style.fontFamily=fontfamily;
+}
+
+function changefontsize(fontsize)
+{
+    var writing=document.getElementById("text");
+    var image1=document.getElementById("img1");
+    var image2=document.getElementById("img2");
+    fontsize=Number(fontsize);
+    if(fontsize<10 || fontsize>50)
+    {
+        a=document.getElementsByName("fontsize");
+        fontsize=25;
+    }
+    writing.style.fontSize=fontsize+'px';
+    image1.style.width=fontsize/2+'%';
+    image1.style.height=fontsize/2+'%';
+    image2.style.width=fontsize/2+'%';
+    image2.style.height=fontsize/2+'%';
+
+}
+
+function changebackgroundcolor(backgroundcolor)
+{
+    var writing=document.getElementById("bg");
+    writing.style.backgroundColor=backgroundcolor;
+}
+
+function back()
+{
+    window.open("周张图书馆.html");
+}
